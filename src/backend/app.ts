054dfd95084826fa242c041ff00e0acd0bd832aa
Arthur Riscado de Morais <arthur.morais@sou.inteli.edu.br>
2026-06-01 15:00:20 -0300

    feat(#269): inicializa a instância do IndexedDB e o armazenamento de objetos sync_queue

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -5,16 +5,27 @@
 
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
+const projectRoot = process.cwd();
 
 // --------------- Middlewares ---------------
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(path.join(projectRoot, 'src/public')));
+
+// --------------- Views ---------------
+app.set('view engine', 'ejs');
+app.set('views', path.join(projectRoot, 'src/views'));
 
 // --------------- Rotas ---------------
 import routes from './routes/index';
+app.get('/', (_req, res) => {
+  res.render('index');
+});
+
 app.use('/api', routes);
 
 export default app;
