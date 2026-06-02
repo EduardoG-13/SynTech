b384b781a65d33b0398e1cbb8d6c885d6a35f843
Eduardo Gabriel de Oliveira <eduardo.oliveira2@sou.inteli.edu.br>
2026-06-02 15:19:50 -0300

    docs(#261): ajusta duplicacao no arquivo app.ts

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,15 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-<<<<<<< HEAD
-import path from 'path';
-=======
 import path from 'path'; // gerencia caminhos fisicos dos arquivos
 import { RETIROS } from './config/retiros';
->>>>>>> 27cf09916327b3c6260fee33ef43bff7b3f12f5c
 
 const app = express();
-const projectRoot = process.cwd();
-
 // === 1. CONFIGURAÇÕES DO EJS (MOTOR DE VISUALIZAÇÃO) ===
 app.set('view engine', 'ejs');
 // Aponta para a pasta "views" subindo um nível a partir de "src/backend" (../views)
@@ -17,15 +11,7 @@ app.set('views', path.join(__dirname, '../views'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-<<<<<<< HEAD
-app.use('/public', express.static(path.join(projectRoot, 'src/public')));
-
-// --------------- Views ---------------
-app.set('view engine', 'ejs');
-app.set('views', path.join(projectRoot, 'src/views'));
-=======
 app.use('/public', express.static(path.join(__dirname, '../public')));
->>>>>>> 27cf09916327b3c6260fee33ef43bff7b3f12f5c
 
 // === 3. SERVIR ARQUIVOS ESTÁTICOS (styles, js, imagens) ===
 // Permite que o navegador acesse os arquivos de "src/public" sob o caminho "/public/..."
@@ -48,7 +34,7 @@ app.get('/tarefas', (req, res) => {
   // Captura o perfil e o retiro da URL (Ex: /tarefas?perfil=Capataz&retiro=Acurizal)
   const perfil = req.query.perfil || 'Capataz';
   const retiro = req.query.retiro || 'Geral';
-  
+
   res.render('tarefas', { perfil, retiro });
 });
 
